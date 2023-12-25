7a64a0cbb75d7e83991f237a7750fac198c5b3eb
Quyến Nguyễn Hùng <21522532@ms.uit.edu.vn>
2023-12-25 17:21:21 +0700

    git commit -m "SE3-168 <Page>"

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home= () => {
+    return (
+        <div>
+            <h1>Home Component</h1>
+            {/* Other login-related content */}
+        </div>
+        );   
+}
+export default Home;
