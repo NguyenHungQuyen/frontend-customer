51e55c7436f9d310a1ba2c329874d8b85ebde760
Quyến Nguyễn Hùng <21522532@ms.uit.edu.vn>
2023-12-26 11:45:42 +0700

    git commit -m "SE3-167 <Authenication screen: Login UI>"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './../assets/css/rectangle.css';
 
+const InputFrame = ({ children }) => {
+    return <div className="input-frame">{children}</div>;
+  };
+ 
+
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -11,9 +17,45 @@ const Login = () => {
     console.log('Logging in...');
   };
 
+  const maskedPassword = '*'.repeat(password.length); 
+
   return (
     <div className="rectangle">
-  
+        <div className="login-wrapper">
+            <div className="login-form">
+                
+            </div>
+
+        <form onSubmit={handleLogin}>
+            <label_Ecomerce> E-commerce</label_Ecomerce>
+            <InputFrame>
+          <label_input>Email:</label_input>
+          <input
+            type="email"
+            placeholder="ellie@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </InputFrame>
+
+        <InputFrame>
+          <label>Password:</label>
+          <input
+           type="text"  // Use type="text" to display the masked password
+           value={maskedPassword}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </InputFrame>
+
+        <button type="submit">
+            <text  >go to store </text>
+            <span>&rarr;</span>
+            </button>    
+
+        </form>
+       </div>
     </div>
   );
 };
