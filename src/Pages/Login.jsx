f8d3a3ee2e9fc8dc0bca9d3d450f9f01ece5e092
Quyến Nguyễn Hùng <21522532@ms.uit.edu.vn>
2023-12-26 22:14:12 +0700

    git commit -m "SE3-167 <Authenication screen: Implement axios to call API>"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './../assets/css/rectangle.css';
+import axios from 'axios';
 
 const InputFrame = ({ children }) => {
     return <div className="input-frame">{children}</div>;
@@ -12,6 +13,33 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleEmail=(e)=>{
+    console.log(e.target.value)
+    setEmail(e.target.value)
+  }
+
+  const handelPassword=(e)=>{
+    console.log(e.target.value)
+    setPassword(e.target.value)
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('submit form');
+    axios.post('http://localhost:3200/login', {
+      email: email,
+      password: password,
+    })
+    .then((response) => {
+      console.log(response.data)
+      alert('sucess login')
+    })
+    .catch((err)=>{
+      console.log(err)
+      console.log(err.response)
+      alert(err.response.data.console.message)
+    })
+  }
+
   const handleLogin = () => {
     // Implement login logic
     console.log('Logging in...');
@@ -21,33 +49,42 @@ const Login = () => {
 
   return (
     <div className='wrappper'>
-     <form action=''>
+     <form action='' onSubmit={handleSubmit}>
       <h1>E-commerce</h1>
       
       <div className='input-box'>
     
-        <input type='text' placeholder='ellie@gmail.com' required></input>
+        <input 
+            type='text' 
+            placeholder='ellie@gmail.com' 
+            required 
+            onChange={handleEmail}
+            value={email}></input>
         <i class='bx bx-envelope'></i>
 
       </div>
 
       <div className='input-box'>
        
-        <input type='password' placeholder='****' required></input>
+        <input 
+            type='password' 
+            placeholder='****' 
+            required 
+            onChange={handelPassword}
+            value={password}
+            ></input>
         <i class='bx bxs-lock-alt' ></i>
       </div>
 
       <div className='remember-forgot'>
-        <label className='remember-forgot-label' >
+        <label>
           <input type="checkbox"></input>
           Remember me
           </label>
           <a href='#'> Forgot Password</a>
       </div>
-    
-      <button type='submit' className='btn' >Login</button>
-    
-    
+
+      <button type='submit' className='btn'>Login</button>
      </form>
     </div>
   );
